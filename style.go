cb11b80bcb95719fabf5236dea704bfbeaac995a
koki-develop <kou.pg.0131@gmail.com>
2026-03-18 11:42:50 +0900

    feat: add ANSI StyleFunc variables

---
+++ style.go
@@ -0,0 +1,34 @@
+package annotate
+
+func ansiStyle(code string) StyleFunc {
+	return func(s string) string {
+		return "\033[" + code + "m" + s + "\033[0m"
+	}
+}
+
+var (
+	Bold      = ansiStyle("1")
+	Dim       = ansiStyle("2")
+	Italic    = ansiStyle("3")
+	Underline = ansiStyle("4")
+)
+
+var (
+	FgRed     = ansiStyle("31")
+	FgGreen   = ansiStyle("32")
+	FgYellow  = ansiStyle("33")
+	FgBlue    = ansiStyle("34")
+	FgMagenta = ansiStyle("35")
+	FgCyan    = ansiStyle("36")
+	FgWhite   = ansiStyle("37")
+)
+
+var (
+	BgRed     = ansiStyle("41")
+	BgGreen   = ansiStyle("42")
+	BgYellow  = ansiStyle("43")
+	BgBlue    = ansiStyle("44")
+	BgMagenta = ansiStyle("45")
+	BgCyan    = ansiStyle("46")
+	BgWhite   = ansiStyle("47")
+)
---
+++ style_test.go
@@ -0,0 +1,40 @@
+package annotate
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAnsiStyleFuncs(t *testing.T) {
+	tests := []struct {
+		name     string
+		fn       StyleFunc
+		input    string
+		expected string
+	}{
+		{"Bold", Bold, "hello", "\033[1mhello\033[0m"},
+		{"Dim", Dim, "hello", "\033[2mhello\033[0m"},
+		{"Italic", Italic, "hello", "\033[3mhello\033[0m"},
+		{"Underline", Underline, "hello", "\033[4mhello\033[0m"},
+		{"FgRed", FgRed, "hello", "\033[31mhello\033[0m"},
+		{"FgGreen", FgGreen, "hello", "\033[32mhello\033[0m"},
+		{"FgYellow", FgYellow, "hello", "\033[33mhello\033[0m"},
+		{"FgBlue", FgBlue, "hello", "\033[34mhello\033[0m"},
+		{"FgMagenta", FgMagenta, "hello", "\033[35mhello\033[0m"},
+		{"FgCyan", FgCyan, "hello", "\033[36mhello\033[0m"},
+		{"FgWhite", FgWhite, "hello", "\033[37mhello\033[0m"},
+		{"BgRed", BgRed, "hello", "\033[41mhello\033[0m"},
+		{"BgGreen", BgGreen, "hello", "\033[42mhello\033[0m"},
+		{"BgYellow", BgYellow, "hello", "\033[43mhello\033[0m"},
+		{"BgBlue", BgBlue, "hello", "\033[44mhello\033[0m"},
+		{"BgMagenta", BgMagenta, "hello", "\033[45mhello\033[0m"},
+		{"BgCyan", BgCyan, "hello", "\033[46mhello\033[0m"},
+		{"BgWhite", BgWhite, "hello", "\033[47mhello\033[0m"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.expected, tt.fn(tt.input))
+		})
+	}
+}
